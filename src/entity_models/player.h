e7eacb75ef41c7798d206c35510021b5d2848901
bl0bb <andreas.hurlen.kristiansen@gmail.com>
2026-08-17 01:40:45 +0200

    offsets

--- src/entity_models/player.h
+++ src/entity_models/player.h
@@ -8,27 +8,27 @@ EntityModel getPlayerModel() {
     EntityModel model(12);
 
     // torso
-    model.elements[0] = EntityElement(8, 12, 4,    5.0, 0, 0,    0, 0, 0,    6);
+    model.elements[0] = EntityElement(8, 12, 4,    -4.0, 12.0, -2.0,    0, 0, 0,    6);
     populateUnwrappedEntityElementFaceUVs(model.elements[0], 16, 16);
 
     // head
-    model.elements[1] = EntityElement(8,  8, 8,    0, 0, 0,    0, 0, 0,    6);
+    model.elements[1] = EntityElement(8,  8, 8,    -4.0, 0.0, -4.0,    0, 0, 0,    6);
     populateUnwrappedEntityElementFaceUVs(model.elements[1], 0, 0);
 
     // right arm
-    model.elements[2] = EntityElement(3, 12, 4,    0, 0, 0,    0, 0, 0,    6);
+    model.elements[2] = EntityElement(3, 12, 4,    0.0, -10.0, -2.0,    0, 0, 0,    6);
     populateUnwrappedEntityElementFaceUVs(model.elements[2], 40, 16);
 
     // left arm
-    model.elements[3] = EntityElement(3, 12, 4,    0, 0, 0,    0, 0, 0,    6);
+    model.elements[3] = EntityElement(3, 12, 4,    -3.0, -10.0, -2.0,    0, 0, 0,    6);
     populateUnwrappedEntityElementFaceUVs(model.elements[3], 32, 48);
 
     // right leg
-    model.elements[4] = EntityElement(4, 12, 4,    0, 0, 0,    0, 0, 0,    6);
+    model.elements[4] = EntityElement(4, 12, 4,    -2.0, -12.0, -2.0,    0, 0, 0,    6);
     populateUnwrappedEntityElementFaceUVs(model.elements[4], 0, 16);
 
     // left leg
-    model.elements[5] = EntityElement(4, 12, 4,    0, 0, 0,    0, 0, 0,    6);
+    model.elements[5] = EntityElement(4, 12, 4,    -2.0, -12.0, -2.0,    0, 0, 0,    6);
     populateUnwrappedEntityElementFaceUVs(model.elements[5], 16, 48);
 
     // clothes
